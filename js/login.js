265b4dd92ea8eb66831f1102b13443d984cf8b76
1tasnem <tasnemprodhan@gmail.com>
2025-03-11 00:53:02 +0600

    login part

--- js/login.js
+++ js/login.js
@@ -4,7 +4,21 @@ document.getElementById('button-login').addEventListener('click',function(event)
     event.preventDefault();
     console.log('button clicked fill added');
 
-    const phoneNumber = document.getElementById('phone-number')
-   console.log(phoneNumber);
-})
+    const phoneNumber = document.getElementById('phone-number').value;
+   const pinNumber = document.getElementById('pin-number').value;
+   console.log(phoneNumber,pinNumber);
+
+   //this is temporary ..you should do like this
+   if(phoneNumber === '5' && pinNumber === '1234')
+   {
+    console.log('you are logged in ');
+    window.location.href = '/home,html';
+
+   }
+   else
+   {
+    alert('wrong phone number or pin number');
+
+   }
+});
 
