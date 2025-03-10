9824bfd560da1af3dfc15c4d9f174c83c4cc4004
1tasnem <tasnemprodhan@gmail.com>
2025-03-11 00:17:06 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,10 @@
+//console.log('button clicked fill added')
+document.getElementById('button-login').addEventListener('click',function(event)
+{
+    event.preventDefault();
+    console.log('button clicked fill added');
+
+    const phoneNumber = document.getElementById('phone-number')
+   console.log(phoneNumber);
+})
+
